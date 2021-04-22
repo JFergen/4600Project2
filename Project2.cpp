348488f1f41f57d86875235d29f6742df2499e13
DrChie <imdatchie@gmail.com>
2021-04-22 11:57:15 -0500

    Removed unused code and added comments

--- Project2.cpp
+++ Project2.cpp
@@ -23,45 +23,41 @@
 #include <algorithm>
 using namespace std;
 
-vector<bool> getReachableSet(vector<vector<int>> inputMatrix, int node)
+vector<bool> getReachableSet(vector<vector<int>> inputMatrix, int node) //This is a BFS algorithm to find the path each node can reach
 {
     vector<bool> ReachableSet;
     ReachableSet.resize(inputMatrix.size(), false);
     queue<int> Queue;
     ReachableSet[node] = true;
-    for (int i = 0; i<inputMatrix.size(); i++)
+
+    for (int i = 0; i<inputMatrix.size(); i++) //This for loop sets up the first entries into the Queue
     {
         if(inputMatrix[node][i] == 1)
         {
             if(ReachableSet[i] == false)
             {
-                Queue.push(i);
+                Queue.push(i); //Push all nodes this node can reach
             }
         }
     }
+
     while(!Queue.empty())
     {
-        node = Queue.front();
-        ReachableSet[node] = true;
-        Queue.pop();
+        node = Queue.front(); //Get the node on the front
+        ReachableSet[node] = true; //Say its reachable
+        Queue.pop(); //Remove it from queue
+
         for (int i = 0; i<inputMatrix.size(); i++)
         {
-            if(inputMatrix[node][i] == 1)
+            if(inputMatrix[node][i] == 1) //If an edge exists
             {
-                if(ReachableSet[i] == false)
+                if(ReachableSet[i] == false) //And its a new unique edge
                 {
-                    Queue.push(i);
+                    Queue.push(i); //Add it to the queue
                 }
             }
         }
     }
-    /*cout<<"These are true: ";
-    for(int i=0; i<ReachableSet.size(); i++)
-    {
-        if(ReachableSet[i]==true)
-        cout<<i<<" ";
-    }
-    cout<<endl;*/
     return ReachableSet;
 }
 
@@ -73,11 +69,10 @@ int main() {
     vector <int> resources;
     vector <vector<int>> inputMatrix;
 
-    fileName = "input-example.txt"; // DELETE THIS AT END
-    // UNCOMMENT THIS AT END
+
     // Get filename from user
-    // cout << "Enter name of file containing data: ";
-    // cin >> fileName;
+    cout << "Enter name of file containing data: ";
+    cin >> fileName;
     fin.open(fileName);
 
     // Check filename and ask for correct file name until it works
@@ -157,39 +152,40 @@ int main() {
     }
     
     vector<bool> KnotSolution, KnotTest;
-    bool Knot = true;
-    bool KnotFound = false;
-    for(int i = 0; i < inputMatrix.size(); i++)
+    bool Knot = true; //This is used to find where the Knot is no longer true
+    bool KnotFound = false; //This is used to see if a Knot fully exists
+    
+    //Knot Detection
+    for(int i = 0; i < inputMatrix.size(); i++) 
     {
-        Knot = true;
-        KnotFound = false;
+        Knot = true; //Both need to be reset
+        KnotFound = false; 
 
-//        cout<<"Finding KnotSolution"<<endl;
-        KnotSolution = getReachableSet(inputMatrix, i);
+        KnotSolution = getReachableSet(inputMatrix, i); //Get everything reachable from i
+
+        //Testing against KnotSolution
         for(int j = 0; j < KnotSolution.size(); j++)
         {
-            if(j != i)
+            if(j != i) //If i and j are the same they have the same reachable set so no need to retest
             {
-//                cout<<"Finding KnotTest"<<endl;
                 KnotTest = getReachableSet(inputMatrix, j);
-                /*Knot = (KnotTest && KnotSolution);
-                cout << Knot <<endl;*/
-                if(KnotTest != KnotSolution)
+                if(KnotTest != KnotSolution) //If the sets aren't the same it's not a knot
                 {
                     Knot = false;
                 }
             }
-            if(Knot == false)
+            if(Knot == false) //If you can prove something isn't a knot at one place, it's not a knot
             {
-                break;
+                break; //So try again
             }
-            if((Knot == true) && (j == (KnotSolution.size()-1)))
+
+            if((Knot == true) && (j == (KnotSolution.size()-1))) //However if the last j is used and Knot is still true
             {
-                KnotFound = true;
+                KnotFound = true; //Then we found a Knot
             }
         }
 
-        if(KnotFound == true)
+        if(KnotFound == true) //Finding one knot is enough
         {
             break;
         }
